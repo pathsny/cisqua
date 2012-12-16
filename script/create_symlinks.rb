b50543ca501aab44162eb490277dd53032a146e6
Vishnu Iyengar <pathsny@gmail.com>
2012-12-16 12:38:02 +0530

    create symlinks now works off a mylist export

--- script/create_symlinks.rb
+++ script/create_symlinks.rb
@@ -2,30 +2,30 @@
 #the anidb ids of anime. the argument passed in is the first folder to start from (to allow resuming)
 require File.expand_path('../../lib/libs', __FILE__)
 options = YAML.load_file(File.expand_path('../../options.yml', __FILE__))
+require 'rexml/document'
 r_options = options[:renamer]
 
-all_folders = Dir["#{r_options[:output_location]}/**"].sort
-first_folder = ARGV.first
-folders = first_folder ? all_folders.drop_while {|k| File.basename(k) != first_folder} : all_folders
-abort "nothing to do" if folders.empty?
+folders = Dir["#{r_options[:output_location]}/**"].sort
+mylist_location = ARGV.first
+mylist = REXML::Document.new File.new("#{mylist_location}/mylist.xml")
 
-client = Anidb.new(options[:anidb])
 renamer = Renamer.new(r_options)
 
 folders.each do |f|
   aid = File.read("#{f}/tvshow.nfo").match(/^aid=(\d+)$/)[1]
-  data = client.anime(aid)[:anime]
-  data[:completed] = client.__send__(:create_mylist_data, aid, data[:episodes].to_i).complete?
-  
-  folder = File.basename f
-  renamer.symlink(f, "#{r_options[:adult_location]}/#{folder}") if r_options[:adult_location] && data[:is_18_restricted] == "1" && !File.symlink?("#{r_options[:adult_location]}/#{folder}")
-  renamer.update_symlinks_for data, folder, f
-  puts "processed #{f}"
+  axml = REXML::Document.new File.new("#{mylist_location}/anime/a#{aid}.xml")
+  renamer.symlink(f, r_options[:adult_location], File.basename(f)) if r_options[:adult_location] && axml.elements["anime/seriesInfo/genres/genre[@id = '80']"]
+  a_attrs = mylist.elements["myList/animeList/anime[@id = '#{aid}']"].attributes
+  ainfo = {:type => a_attrs["type"],
+  :ended => true,
+  :completed => a_attrs["status"] == 'complete'}
+  renamer.update_symlinks_for ainfo, File.basename(f), f
+  puts "#{f} has #{ainfo.inspect}"
 end
 
 syms = r_options[:create_symlinks]
 symlink_folders = [:movies, :incomplete_series, :complete_series, :incomplete_other, :complete_other].map { |k|
   Dir["#{syms[k]}/**"].map {|f| File.basename(f)}
 }.reduce([]) {|acc, arr| acc | arr}
-all_folder_names = all_folders.map {|f| File.basename(f)}
+all_folder_names = folders.map {|f| File.basename(f)}
 puts "something is wrong #{all_folder_names} does not map to #{symlink_folders}" unless symlink_folders.size == all_folder_names.size && (symlink_folders & all_folder_names == symlink_folders)
