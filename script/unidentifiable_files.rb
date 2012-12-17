2c0749e547b5296c5527abc4b23accfb61c4f88f
Vishnu Iyengar <pathsny@gmail.com>
2012-12-17 10:57:22 +0530

    added script to find files that dont fit the naming patterns

---
+++ script/unidentifiable_files.rb
@@ -0,0 +1,34 @@
+require File.expand_path('../../lib/libs', __FILE__)
+options = YAML.load_file(File.expand_path('../../options.yml', __FILE__))
+require 'rexml/document'
+r_options = options[:renamer]
+
+mylist_location = ARGV.first
+mylist = REXML::Document.new File.new("#{mylist_location}/mylist.xml")
+
+def m_pattern(folder_name)
+  extension = /\.[a-z0-9]+$/
+  end_part = /\s- (?:Complete Movie|Part \d+ of \d+)(?:\s\[[\w-]+\])? \[\(X-\d+\)\]/
+  [Regexp.new("^#{folder_name}#{end_part.source}#{extension.source}")] 
+end
+def s_pattern(folder_name)
+  extension = /\.[a-z0-9]+$/
+  end_part = /\s- episode \d+(?:\s\[[\w-]+\])?/
+  special_end = /\s- episode [A-Z](\d+)(?:\s\[[\w-]+\])?\s\[\(XS-\d+-\1\)\]/
+  [Regexp.new("^#{folder_name}#{end_part.source}#{extension.source}"), 
+    Regexp.new("^#{folder_name}#{special_end.source}#{extension.source}")]
+end    
+
+all_folders = Dir["#{r_options[:output_location]}/**"].sort
+all_folders.each do |folder|
+  aid = File.read("#{folder}/tvshow.nfo").match(/^aid=(\d+)$/)[1]
+  folder_name = File.basename(folder)
+  files = Dir["#{folder}/*"].map {|a| File.basename(a)}.reject {|f| /^[^\.]*$/.match f} - ["tvshow.nfo"]
+  movie = mylist.elements["myList/animeList/anime[@id = '#{aid}']"].attributes['type'] == 'Movie'
+  patterns = movie ? m_pattern(folder_name) : s_pattern(folder_name)
+  files.each do |file| 
+    puts "do not know file #{file} of #{folder}" unless patterns.any?{|p| p.match file}
+  end  
+end
+
+  
