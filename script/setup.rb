eec1e44c7b207d9988d1b1278a68d71b34bfb695
Vishnu S Iyengar <pathsny@gmail.com>
2015-03-22 20:36:39 -0700

    bug fix in setup

--- script/setup.rb
+++ script/setup.rb
@@ -7,8 +7,9 @@ Dir.chdir(root_dir) {
   raise "could not run bundler" unless system "bundle install"
 }
 
-if ARGV[0] == 'dev' {
+if ARGV[0] == 'dev' then
   Dir.chdir(File.join(root_dir, 'web')) {
     raise "could not run bower" unless system "bower install"
-  }  
-}
+  }
+end
+
