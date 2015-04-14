d88a393e82d22d4fca1c6cf617ecc886b47acd81
Vishnu S Iyengar <pathsny@gmail.com>
2015-04-14 09:59:02 -0700

    remove spurious connect

--- script/anidb_console.rb
+++ script/anidb_console.rb
@@ -2,4 +2,3 @@ require 'yaml'
 Options = YAML.load_file File.expand_path('../../options.yml', __FILE__)
 require File.expand_path('../../lib/libs', __FILE__)
 Client = Net::AniDBUDP.new(*([:host, :port, :localport, :user, :pass, :nat].map{|k| Options[:anidb][k]}))
-Client.connect 
