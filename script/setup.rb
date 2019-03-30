e40f2aa32e1ccb08d1782212063528661e3341ac
Vishnu Iyengar <vsi@fb.com>
2019-03-30 01:06:55 -0700

    modify setup to store log folder

--- script/setup.rb
+++ script/setup.rb
@@ -7,11 +7,13 @@ options_file = File.join(data_dir, 'options.yml')
 db_dir = File.join(data_dir, 'db')
 http_cache_dir = File.join(data_dir, 'http_anime_info_cache')
 http_cache_lock_dir = File.join(data_dir, 'http_anime_info_cache/lock')
+log_dir = File.join(root_dir, 'log')
 
 FileUtils.mkdir_p(data_dir)
 FileUtils.mkdir_p(db_dir)
 FileUtils.mkdir_p(http_cache_dir)
 FileUtils.mkdir_p(http_cache_lock_dir)
+FileUtils.mkdir(log_dir)
 
 FileUtils.cp(File.join(root_dir, 'script/helpers/options.yml.bak'), options_file) unless File.exist?(options_file)
 
@@ -24,4 +26,3 @@ if ARGV[0] == 'dev' then
     raise "could not run npm" unless system "npm install"
   }
 end
-
