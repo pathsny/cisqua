aa3f43592c022add4fc8a4557b1d5f9476f0a24b
Vishnu Iyengar <vsi@fb.com>
2019-11-30 23:56:51 -0800

    stop the npm packaging since I'm not using it anymore. fix deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.11.0'
+lock '3.11.2'
 
 set :application, 'anidb'
 set :repo_url, '.'
@@ -42,25 +42,11 @@ namespace :rsync do
       Dir.chdir public_dir do
         FileUtils.mkdir_p shared_node_modules_dir
         File.symlink(shared_node_modules_dir, current_node_modules_dir) unless File.symlink?(current_node_modules_dir)
-        system("npm install && npm run build")
+        # system("npm install && npm run build")
       end
     end
 end
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('data', 'tmp/pids', 'tmp/sockets', 'log')
 
 set :puma_rackup, -> { File.join(current_path, 'web', 'config.ru') }
@@ -74,17 +60,26 @@ set :puma_rackup, -> { File.join(current_path, 'web', 'config.ru') }
 namespace :deploy do
 
   def ensure_dir(dir_path)
-    unless test(dir_path)
+    unless test("[ -d #{dir_path} ]")
       execute "mkdir -p #{dir_path}"
     end
   end
 
+  namespace :check do
+    after :directories, :setup_shared_infra  do
+      on roles(:all) do
+        data_path = File.join(shared_path, "data")
+        ensure_dir File.join(data_path, "db")
+        ensure_dir File.join(data_path, "http_anime_info_cache")
+        ensure_dir File.join(data_path, "udp_anime_info_cache/lock")
 
-  after :check, :create_directories do
-    on roles(:app) do
-      ensure_dir File.join(shared_path, "data/db")
-      ensure_dir File.join(shared_path, "data/http_anime_info_cache")
-      ensure_dir File.join(shared_path, "data/udp_anime_info_cache/lock")
+
+        options_path = File.join(data_path, "options.yml")
+        unless test("[ -f #{options_path} ]")
+          local_options_bak = File.expand_path('../../script/helpers/options.yml.bak', __FILE__)          puts "coping options #{local_options_bak}"
+          upload! local_options_bak, options_path
+        end
+      end
     end
   end
 
