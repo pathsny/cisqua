901c9e53381b873a0c226263978f74c8171de825
Vishnu Iyengar <vsi@fb.com>
2019-12-31 11:23:12 -0800

    fix the way we require files to avoid double requiring when the app is invoked from a directory that contains a symlinked directory

--- lib/libs.rb
+++ lib/libs.rb
@@ -1,8 +1,7 @@
 require 'yaml'
 require 'thread'
-require_relative './loggers'
 require_relative '../external/lru_hash'
 
-Dir[File.join(File.dirname(__FILE__), '**/*.rb')].each do |f| 
+Dir[File.join(__dir__, '**/*.rb')].each do |f|
   require f unless f == __FILE__
 end
