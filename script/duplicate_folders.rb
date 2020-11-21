2559a5229d855c3c4cd44d77c2c4962b80d9184d
Vishnu Iyengar <vsi@fb.com>
2020-11-21 08:31:32 -0800

    better error detectio when finding duplicate folders

--- script/duplicate_folders.rb
+++ script/duplicate_folders.rb
@@ -16,7 +16,11 @@ options = ScriptOptions.load_options(options_file)
 r_options = options[:renamer]
 
 all_folders = Dir["#{r_options[:output_location]}/**"].sort
-aid_folder_hash = all_folders.group_by {|f| File.read("#{f}/tvshow.nfo").match(/^aid=(\d+)$/)[1]}
+aid_folder_hash = all_folders.group_by do |f|
+  match_data = File.read("#{f}/tvshow.nfo").match(/^aid=(\d+)$/)
+  raise "did not match for #{f}" unless match_data
+  match_data[1]
+end
 aid_folder_hash.each {
   |aid, folders| Loggers::Duplicates.info { "duplicate #{folders.inspect}" } if folders.length > 1
 }
